7385df1ce9546e4537544285e26f3223e1ce24a6
HeitorDalla <heitorvillavilla@gmail.com>
2025-12-02 22:09:10 -0300

    refatoracao: refatorando a service do Pedido, excluindo duplicatas e arrumando a ordems dos metodos para organizacao do codigo

--- src/main/java/br/edu/unicesumar/service/PedidoService.java
+++ src/main/java/br/edu/unicesumar/service/PedidoService.java
@@ -12,10 +12,12 @@ import br.edu.unicesumar.model.Pedido;
 public class PedidoService {
     private PedidoDAO pedidoDAO;
     private UsuarioService usuarioService;
+    private ItemPedidoService itemPedidoService;
 
     public PedidoService () {
         this.pedidoDAO = new PedidoDAO();
         this.usuarioService = new UsuarioService();
+        this.itemPedidoService = new ItemPedidoService();
     }
 
     // Salva o pedido e envia para o DAO armazenar/validar no banco de dados
@@ -24,14 +26,6 @@ public class PedidoService {
 
         // Se todos as validações forem corretas, ele chama o DAO para salvar no banco de dados
         pedidoDAO.save(pedido);
-    }
-
-    // Adiciona o itemPedido ao pedido
-    public void addItemPedido (Pedido pedido, ItemPedido itemPedido) throws BusinessException {
-        // Validar item antes de adicionar
-        validarItemPedido(itemPedido);
-        
-        pedido.addItemPedido(itemPedido);
     }
 
     // Valida o pedido antes de salvar no banco de dados (DAO)
@@ -48,19 +42,12 @@ public class PedidoService {
         usuarioService.validarUsuario(pedido.getUsuario());
     }
 
-    // Valida as informacoes do item antes de adicionar ao pedido
-    public void validarItemPedido (ItemPedido itemPedido) throws BusinessException {
-        if (itemPedido == null) {
-            throw new BusinessException("Item de pedido não pode ser nulo.");
-        }
+    // Adiciona o itemPedido ao pedido
+    public void addItemPedido (Pedido pedido, ItemPedido itemPedido) throws BusinessException {
+        // Validar item antes de adicionar
+        itemPedidoService.validarItemPedido(itemPedido);
         
-        if (itemPedido.getProduto() == null) {
-            throw new BusinessException("Produto do item não pode ser nulo.");
-        }
-        
-        if (itemPedido.getQuantidade() <= 0) {
-            throw new BusinessException("Quantidade do item deve ser maior que zero.");
-        }
+        pedido.addItemPedido(itemPedido);
     }
 
     // Recebe o id do Controller, busca o Pedido no DAO e retorna o resultado ao Controller
